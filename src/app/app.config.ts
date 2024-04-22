396f0e7ce03ca92417c94448d8124a9b252a8253
mike <mike_sirb@yahoo.de>
2024-04-22 20:55:05 +0200

    init/kbb57: install necessary dependencies for ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore(), provideEffects(), provideRouterStore()],
 };
