98200405b22086b4692fa2b99532793a7545337d
mike <mike_sirb@yahoo.de>
2024-04-23 18:44:10 +0200

    feat/kbb-32: route for admin document-management and nav-button

---
+++ src/app/admin-view/documents/document-management/document-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './document-management.component.html',
+  styleUrl: './document-management.component.scss'
+})
+export class DocumentManagementComponent {
+
+}
