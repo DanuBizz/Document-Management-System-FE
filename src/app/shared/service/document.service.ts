8ff9473f398121a917ecf2e867aed947ff60f648
mike <mike_sirb@yahoo.de>
2024-04-26 15:17:18 +0200

    feat/kbb-97: Creating environments (prod/dev) with default values for further steps

--- src/app/shared/service/document.service.ts
+++ src/app/shared/service/document.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, map, Observable } from 'rxjs';
 import { DocumentResponseInterface } from '../../admin-view/type/document-response.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentService {
-  private documentsBaseUrl: string = 'http://localhost:8080/documents';
+  private baseUrl: string = environment.apiUrl + '/documents';
   private queryParam: string = '?page=0&size=20&sort=asc';
 
   constructor(private http: HttpClient) {}
@@ -17,7 +18,7 @@ export class DocumentService {
    * @returns An observable of an array of document response objects.
    */
   getDocuments(): Observable<DocumentResponseInterface[]> {
-    const fullUrl = this.documentsBaseUrl + this.queryParam;
+    const fullUrl = this.baseUrl + this.queryParam;
     return this.http.get<{ content: DocumentResponseInterface[] }>(fullUrl).pipe(
       delay(3000), // Simulate delay for demonstration purposes
       map(response => response.content)
