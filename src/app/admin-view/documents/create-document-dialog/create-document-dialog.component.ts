fb55f15dc84ee060d5928562f5f1bf494b7377cc
mike <mike_sirb@yahoo.de>
2024-04-28 11:55:58 +0200

    feat/kbb-99: implementing button to open browser and select document

--- src/app/admin-view/documents/create-document-dialog/create-document-dialog.component.ts
+++ src/app/admin-view/documents/create-document-dialog/create-document-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -6,17 +6,18 @@ import {
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatOption, MatSelect } from '@angular/material/select';
-import { MatInput } from '@angular/material/input';
-import { MatButton } from '@angular/material/button';
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { CommonModule } from '@angular/common';
-import { selectCategoryData } from '../../categories/store/category.reducers';
-import { DocumentResponseInterface } from '../../type/document-response.interface';
-import { CategoryResponseInterface } from '../../type/category-response.interface';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {MatInput} from '@angular/material/input';
+import {MatButton, MatMiniFabButton} from '@angular/material/button';
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {CommonModule} from '@angular/common';
+import {selectCategoryData} from '../../categories/store/category.reducers';
+import {DocumentResponseInterface} from '../../type/document-response.interface';
+import {CategoryResponseInterface} from '../../type/category-response.interface';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-create-document-dialog',
@@ -33,6 +34,9 @@ import { CategoryResponseInterface } from '../../type/category-response.interfac
     MatInput,
     MatButton,
     MatLabel,
+    MatSuffix,
+    MatIcon,
+    MatMiniFabButton
   ],
   templateUrl: './create-document-dialog.component.html',
   styleUrl: './create-document-dialog.component.scss',
@@ -49,6 +53,9 @@ export class CreateDocumentDialogComponent implements OnInit {
 
   // Observable for retrieving categories from the store
   categories$: Observable<CategoryResponseInterface[]> = this.store.select(selectCategoryData);
+
+  // contains the selected file object
+  private file!: File;
 
   /**
    * @param fb - The FormBuilder service for creating form controls and groups.
@@ -81,7 +88,7 @@ export class CreateDocumentDialogComponent implements OnInit {
     this.form = this.fb.group({
       name: [{ value: this.formName, disabled: this.isDisabled }, this.isDisabled ? [] : Validators.required],
       version: ['', Validators.required],
-      filePath: ['', Validators.required],
+      fileName: ['', Validators.required],
       categories: ['', Validators.required],
     });
   }
@@ -99,17 +106,45 @@ export class CreateDocumentDialogComponent implements OnInit {
    * Otherwise, combines the form data with the document name and closes the dialog.
    */
   save() {
+
+    let data = {
+      ...this.form.value,
+      file: this.file
+    };
+
     // If a document exists, close the dialog with the form value
     if (this.document !== null) {
-      this.dialogRef.close(this.form.value);
+      this.dialogRef.close(data);
     }
 
     // Otherwise, combine the form data with the document name and close the dialog
-    const newData = {
+    data = {
       name: this.formName,
       ...this.form.value,
+      file: this.file
     };
 
-    this.dialogRef.close(newData);
+    this.dialogRef.close(data);
   }
+
+  /**
+   * Handles the selection of a file from the file input.
+   * Updates the form control and displays the selected file name.
+   *
+   * @param event The event object triggered by selecting a file.
+   */
+  selectFile(event: Event): void {
+    // Check if the event target is an HTMLInputElement and if files are selected
+    if (event.target instanceof HTMLInputElement && event.target.files && event.target.files.length > 0) {
+      // Get the first selected file
+      this.file = event.target.files[0];
+      // Patch the form value with the selected file name
+      this.form.patchValue({
+        fileName: this.file.name
+      });
+    }
+  }
+
+
+
 }
